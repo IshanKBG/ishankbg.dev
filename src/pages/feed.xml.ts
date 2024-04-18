7bd42b940dc191228e9e8efcf1b48de4cb2660cd
Ishan Jaiswal <jaiswalishan701@gmail.com>
2024-04-18 11:17:53 +0530

    add more features to rss feed

--- src/pages/feed.xml.ts
+++ src/pages/feed.xml.ts
@@ -1,17 +1,23 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 import type {APIContext} from "astro";
-
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+const parser = new MarkdownIt();
 export const GET  = async (context: APIContext) => {
     const blog = await getCollection('blog');
     return rss({
         title: "Ishan's Blog",
         description: "My blog's rss feed",
+        stylesheet: "/styles.xsl",
         site: context.site!,
         items: blog.map((post) => ({
             title: post.data.title,
             pubDate: post.data.pubDate,
             description: post.data.description,
+            content: sanitizeHtml(parser.render(post.body), {
+                allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
+            }),
             customData: post.data.customData,
             link: `/blog/${post.slug}/`,
         })),
