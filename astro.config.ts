9c5ebc6255f22462d52339b0f4af5c2b731a7ca1
Ishan Jaiswal <jaiswalishan701@gmail.com>
2024-04-18 12:26:20 +0530

    add prefetch tag

--- astro.config.ts
+++ astro.config.ts
@@ -7,6 +7,7 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://ishankbg.dev",
+  prefetch: true,
   integrations: [tailwind(), mdx(), sitemap()],
   markdown: {
     shikiConfig: {
