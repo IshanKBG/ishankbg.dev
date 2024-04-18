de209368d4923a2cbaef21432f20236b88e5fb4a
Ishan Jaiswal <jaiswalishan701@gmail.com>
2024-04-18 11:17:08 +0530

    refactor pages

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,21 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://ishankbg.dev",
-    integrations: [tailwind()],
-    image: {
-        domains: ['images.unsplash.com', 'cdn.discordapp.com']
+  site: "https://ishankbg.dev",
+  integrations: [tailwind(), mdx(), sitemap()],
+  markdown: {
+    shikiConfig: {
+      theme: 'vitesse-dark',
+      wrap: true
     }
+  },
+  image: {
+    domains: ['images.unsplash.com', 'cdn.discordapp.com']
+  }
 });
