058be551a215f2b0e7160fd273dfeadc62009cb2
Ishan Jaiswal <jaiswalishan701@gmail.com>
2024-05-16 08:04:27 +0530

    feat: add tweet link parser

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,59 @@
-use axum::{
-    extract::{Path, State},
-    http::StatusCode,
-    response::{Html, IntoResponse, Response},
-    routing::get,
-    Router,
-};
-use jotdown::Render;
+
+use std::fs::File;
+use std::io::Read;
+use std::str::from_utf8;
+use regex::Regex;
+fn parse_tweet_link(text: &str) -> Vec<(String, String)> {
+  let re = Regex::new(r"https?://twitter.com/(?P<username>[^/]+)/status/(?P<tweet_id>\d+)").unwrap();
+  let captures = re.captures_iter(text);
+
+  let mut results = Vec::new();
+  for cap in captures {
+    let username = cap.name("username").unwrap().as_str().to_owned();
+    let tweet_id = cap.name("tweet_id").unwrap().as_str().to_owned();
+    results.push((username, tweet_id));
+  }
+  results
+}
+
+fn parse_markdown_to_html(file_path: &str) -> Result<String, String> {
+  let mut file = match File::open(file_path) {
+    Ok(f) => f,
+    Err(err) => return Err(format!("Error opening file: {}", err)),
+  };
+
+  let mut content = Vec::new();
+  match file.read_to_end(&mut content) {
+    Ok(_) => (),  // No specific action needed in successful read
+    Err(err) => return Err(format!("Error reading file: {}", err)),
+  }
+
+  let string_content = match from_utf8(&content) {
+    Ok(s) => s,
+    Err(err) => return Err(format!("Invalid UTF-8 content: {}", err)),
+  };
+
+  let mut html_output = markdown::to_html_with_options(
+            string_content,
+            &markdown::Options::gfm()
+        );
+
+  Ok(html_output.unwrap())
+}
+
 fn main() {
-    println!("Hello, world!");
+  let file_path = "content/react-server-components-a-bad-idea.md";
+  let markdown = parse_markdown_to_html(file_path).unwrap(); 
+  println!("{}", &markdown);
+  let tweet_links = parse_tweet_link(&markdown);
+  if tweet_links.is_empty() {
+    println!("No tweet links found");
+  } else {
+    println!("Found tweet links:");
+    for (username, tweet_id) in tweet_links.iter() {
+      println!("  Username: {}", username);
+      println!("  Tweet ID: {}", tweet_id);
+    }
+  }
 }
-async fn parser(State(state): State<AppState>, Path(page): Path<String>) -> impl IntoResponse {
-    let mut file = format!("content/{page}.md");
 
-    let markdown_input = match tokio::fs::read(file).await {
-        Ok(res) => res,
-        Err(_) => return Html("Couldn't find this page - does it exist?".to_string()),
-    };
-
-    let string_output = std::str::from_utf8(&markdown_input).unwrap();
-    let mut html_output = String::new();
-    let parser = jotdown::Parser::new(string_output); 
-    
-    jotdown::html::Renderer::default().push(&parser, &mut html_output);
-
-    Html(html_output)
-}
