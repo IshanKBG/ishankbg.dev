4b2a6eb666902dc9646824b0775bc43907107f4e
Ishan Jaiswal <jaiswalishan701@gmail.com>
2024-06-04 13:55:21 +0530

    feat: remove unused packages

--- astro.config.ts
+++ astro.config.ts
@@ -2,15 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import markdoc from "@astrojs/markdoc";
 import embed from "astro-embed/integration"
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://ishankbg.dev",
 	prefetch: true,
-	integrations: [tailwind(), sitemap(), markdoc(), embed({
-	}), mdx()],
+	integrations: [tailwind(), sitemap(), embed(), mdx()],
 	markdown: {
 		shikiConfig: {
 			theme: 'vitesse-dark',
