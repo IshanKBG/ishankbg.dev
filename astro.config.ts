ca09e7890400e17f32248b0f2407146223623f3d
Ishan Jaiswal <jaiswalishan701@gmail.com>
2024-06-04 13:50:27 +0530

    feat: refactoring

--- astro.config.ts
+++ astro.config.ts
@@ -3,12 +3,14 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import markdoc from "@astrojs/markdoc";
+import embed from "astro-embed/integration"
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://ishankbg.dev",
 	prefetch: true,
-	integrations: [tailwind(), mdx(), sitemap(), markdoc()],
+	integrations: [tailwind(), sitemap(), markdoc(), embed({
+	}), mdx()],
 	markdown: {
 		shikiConfig: {
 			theme: 'vitesse-dark',
